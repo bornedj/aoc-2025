72145897b8051b6fd2948fd4e21881308b322bff
Daniel Borne <borne.danielj@gmail.com>
2026-07-11 11:24:45 -0400

    feat: complete puzzle on of day one
    somewhat sloppy solution with basically no error handling. Uses
    rem_euclid to handle the revolutions of the lock
    
    wip incorrect computed value
    
    need to account for rotations greater than 99
    
    working impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
+use std::{error::Error, fs, path::Path};
+
+#[derive(Debug)]
+enum Direction {
+    Left(i16),
+    Right(i16)
+}
+
+fn direction(line: &str) -> Direction {
+    if let Some(distance) = line.strip_prefix('L') {
+        let distance = distance.parse::<i16>().expect("must be a parseable number input");
+        return Direction::Left(distance)
+    } else if let Some(distance) = line.strip_prefix('R') {
+        let distance = distance.parse::<i16>().expect("must be a parseable number input");
+        return Direction::Right(distance)
+    } else {
+        panic!("unexpected prefix")
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
+
+    let file = fs::read_to_string(Path::new("/home/daniel/github/advent-of-code-2025/src/assets/day-one-puzzle-one-input.txt"))?;
+    let directions: Vec<Direction> = file.lines().map(direction).collect();
+
+    let mut position = 50;
+    let mut count = 0;
+
+    directions.iter().for_each(|direction| {
+        match direction {
+            Direction::Left(num) => position = (position - num).rem_euclid(100),
+            Direction::Right(num) => position = (position + num).rem_euclid(100)
+        };
+        if position == 0 {
+            count += 1;
+        }
+    });
+
+    println!("{count}");
+    Ok(())
 }
