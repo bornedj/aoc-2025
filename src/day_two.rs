dc42125319664e5ddce5e8b254df9a2682982157
Daniel Borne <borne.danielj@gmail.com>
2026-07-11 14:41:16 -0400

    feat: passing fn tests

--- src/day_two.rs
+++ src/day_two.rs
@@ -1,24 +1,42 @@
 use std::ops::RangeInclusive;
 
-fn process() {}
+fn process_day_two(input: &str) -> u32 {
+    let string_ranges = parse_delimited_string(input);
+    let ranges = string_ranges.into_iter().map(calculate_ranges);
+    ranges.map(process_ranges).sum()
+}
 
 fn parse_delimited_string(s: &str) -> Vec<&str> {
     s.split(',').collect()
 }
 
-fn compute_range(s: &str) -> RangeInclusive<u16> {
+fn calculate_ranges(s: &str) -> RangeInclusive<u32> {
     let mut iter = s.split('-');
     let first = iter.next().expect("Must be two numbers per comma delimination").parse().expect("All inputs should parse to ints");
     let last = iter.next().expect("Must be two numbers per comma delimination").parse().expect("All inputs should parse to ints");
     first..=last
 }
 
-fn check_doubles() {}
+fn process_ranges(range: RangeInclusive<u32>) -> u32 {
+    range
+        .filter(|num| contains_repeated_substr(num.to_string()))
+        .sum()
+}
+
+fn contains_repeated_substr(input: String) -> bool {
+    if input.len() % 2 != 0 {
+        return false
+    }
+    let half_way_point = input.len() / 2;
+    let first_half = &input[0..half_way_point];
+    let second_half = &input[half_way_point..];
+    first_half == second_half
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
 
     #[test]
     fn parse_sample_input() {
@@ -27,8 +45,25 @@ mod tests {
     }
 
     #[test]
-    fn test_compute_range() {
+    fn test_calculate_ranges() {
         let input = "11-22";
-        assert_eq!((11..=22), compute_range(input));
+        assert_eq!((11..=22), calculate_ranges(input));
+    }
+
+    #[test]
+    fn test_count_repeated_substr() {
+        assert_eq!(contains_repeated_substr("11".to_string()), true);
+        assert_eq!(contains_repeated_substr("22".to_string()), true);
+        assert_eq!(contains_repeated_substr("1010".to_string()), true);
+        assert_eq!(contains_repeated_substr("1188511885".to_string()), true);
+        assert_eq!(contains_repeated_substr("222222".to_string()), true);
+        assert_eq!(contains_repeated_substr("446446".to_string()), true);
+        assert_eq!(contains_repeated_substr("38593859".to_string()), true);
+    }
+
+    #[test]
+    fn test_process() {
+        let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+        assert_eq!(process_day_two(input), 1227775554);
     }
 }
