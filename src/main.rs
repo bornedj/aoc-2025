ff00352ae09aa8a751be1dd0c5ed6325f3f0eb4b
Daniel Borne <borne.danielj@gmail.com>
2026-07-11 13:46:07 -0400

    refactor: move the first day to a module

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::{error::Error, fs, path::Path};
-use advent_of_code_2025::{Direction, process_puzzle_one, process_puzzle_two};
+use advent_of_code_2025::day_one::{Direction, process_puzzle_one, process_puzzle_two};
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
 
     let file = fs::read_to_string(Path::new("/home/daniel/github/advent-of-code-2025/src/assets/day-one-puzzle-one-input.txt"))?;
-    let directions: Vec<Direction> = file.lines().map(advent_of_code_2025::direction).collect();
+    let directions: Vec<Direction> = file.lines().map(advent_of_code_2025::day_one::direction).collect();
 
     let position = 50;
     let mut count = 0;
