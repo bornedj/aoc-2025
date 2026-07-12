b7e766e4c839d42293fa28999602396a1fa84051
Daniel Borne <borne.danielj@gmail.com>
2026-07-12 13:33:14 -0400

    refactor: extract some shared code

--- src/days/day_six.rs
+++ src/days/day_six.rs
@@ -10,13 +10,17 @@ pub fn process_puzzle_one(input: &str) -> u64 {
         .pop()
         .expect("Expected operators row at the end");
 
+    iterate_operators(&operators, &home_work_sheet)
+}
+
+fn iterate_operators(operators: &Vec<Token>, sheet: &Vec<Vec<Token>>) -> u64 {
     let mut total = 0;
 
     for (i, operator) in operators.iter().enumerate() {
         match operator {
             Token::Number(_) => panic!("Expected operator, found number"),
             Token::Operator(op) => {
-                let col = extract_col(&home_work_sheet, i);
+                let col = extract_col(sheet, i);
                 match op {
                     '*' => total += multiply_col(&col),
                     '+' => total += add_col(&col),
@@ -40,6 +44,14 @@ fn add_col(col: &Vec<&Token>) -> u64 {
         Token::Operator(_) => panic!("Should only be numbers left in the matrix"),
         Token::Number(num) => acc + num,
     })
+}
+
+fn vertical_multiply_col(col: &Vec<&Token>) {
+    todo!()
+}
+
+fn vertical_add_col(col: &Vec<&Token>) {
+    todo!()
 }
 
 fn extract_col(sheet: &Vec<Vec<Token>>, col_index: usize) -> Vec<&Token> {
@@ -82,5 +94,9 @@ mod tests {
     #[test]
     fn puzzle_one_example_input() {
         assert_eq!(process_puzzle_one(EXAMPLE_INPUT), 4277556);
+    }
+
+    #[test]
+    fn puzzle_two_example_input() {
     }
 }
