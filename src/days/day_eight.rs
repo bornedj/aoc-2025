cb5c65f581e1c5bc0688fbed27cbc20cc92f805c
Daniel Borne <borne.danielj@gmail.com>
2026-07-17 18:03:36 -0400

    feat: solve puzzle two
    Squashes:
    
    feat: pass puzzle two test
    
    style: cargo fmt
    
    feat: add the day two input
    
    chore: clippy fixes
    
    fix: refactor to i64 to get around overflow

--- src/days/day_eight.rs
+++ src/days/day_eight.rs
@@ -2,26 +2,20 @@ use std::{collections::HashMap, usize};
 
 #[derive(Debug, PartialEq, Hash, Eq, Copy, Clone)]
 struct Coordinate {
-    x: i32,
-    y: i32,
-    z: i32,
+    x: i64,
+    y: i64,
+    z: i64,
 }
 
 trait ComputeDistance {
-    fn compute_distance(&self, other: &Self) -> f64;
+    fn compute_distance(&self, other: &Self) -> i64;
 }
 
 impl ComputeDistance for Coordinate {
-    fn compute_distance(&self, other: &Self) -> f64 {
-        let floating_self_x = f64::from(self.x);
-        let floating_self_y = f64::from(self.y);
-        let floating_self_z = f64::from(self.z);
-        let floating_other_x = f64::from(other.x);
-        let floating_other_y = f64::from(other.y);
-        let floating_other_z = f64::from(other.z);
-
-        let exponent: f64 = f64::from(2);
-        ((floating_self_x - floating_other_x).powf(exponent) + (floating_self_y - floating_other_y).powf(exponent) + (floating_self_z - floating_other_z).powf(exponent)).sqrt()
+    fn compute_distance(&self, other: &Self) -> i64 {
+        i64::isqrt(
+            (self.x - other.x).pow(2) + (self.y - other.y).pow(2) + (self.z - other.z).pow(2),
+        )
     }
 }
 
@@ -30,20 +24,76 @@ pub fn puzzle_one(input: &str, junction_count: usize) -> u32 {
     let sorted_coord_distance = coordinates_vec_to_sorted_distance_vec(&coordinates);
     let circuits = create_circuits(sorted_coord_distance, junction_count);
 
-    let mut counts: Vec<usize> = circuits.values().fold(HashMap::new(), |mut acc, &id| {
-        *acc.entry(id).or_insert(0) += 1;
-        acc
-    })
-    .into_values()
-    .collect();
+    let mut counts: Vec<usize> = circuits
+        .values()
+        .fold(HashMap::new(), |mut acc, &id| {
+            *acc.entry(id).or_insert(0) += 1;
+            acc
+        })
+        .into_values()
+        .collect();
 
-    counts.sort_by(|a,b| b.cmp(&a));
+    counts.sort_by(|a, b| b.cmp(a));
     counts.iter().take(3).map(|&count| count as u32).product()
 }
 
+pub fn puzzle_two(input: &str) -> i64 {
+    let coordinates = map_input_to_coordinates(input);
+    let sorted_coord_distance = coordinates_vec_to_sorted_distance_vec(&coordinates);
+    let last_junction = find_last_junction(&coordinates, sorted_coord_distance);
+    last_junction.0.x * last_junction.1.x
+}
+
+fn find_last_junction<'a>(
+    all_coordinates: &Vec<Coordinate>,
+    distance_map: Vec<((&'a Coordinate, &'a Coordinate), i64)>,
+) -> (&'a Coordinate, &'a Coordinate) {
+    let mut circuit_map: HashMap<&Coordinate, usize> = HashMap::new();
+    for (coords, _) in distance_map.iter() {
+        let zero_index = circuit_map.get(coords.0).copied();
+        let one_index = circuit_map.get(coords.1).copied();
+        match (zero_index, one_index) {
+            (None, None) => {
+                if let Some(max_index) = circuit_map.values().max().copied() {
+                    circuit_map.insert(coords.0, max_index + 1);
+                    circuit_map.insert(coords.1, max_index + 1);
+                } else {
+                    circuit_map.insert(coords.0, 0);
+                    circuit_map.insert(coords.1, 0);
+                }
+            }
+            (Some(zero_index), None) => {
+                circuit_map.insert(coords.1, zero_index);
+            }
+            (None, Some(one_index)) => {
+                circuit_map.insert(coords.0, one_index);
+            }
+            (Some(zero_index), Some(one_index)) => {
+                for circuit_id in circuit_map.values_mut() {
+                    if circuit_id == &zero_index {
+                        *circuit_id = one_index;
+                    }
+                }
+            }
+        }
+        let first = circuit_map.values().next().unwrap();
+        if all_coordinates.iter().all(|coord| {
+            if let Some(index) = circuit_map.get(coord) {
+                return index == first;
+            } else {
+                false
+            }
+        }) {
+            println!("{:?}", coords);
+            return *coords;
+        }
+    }
+    panic!("didn't find last connection")
+}
+
 fn create_circuits<'a>(
-    sorted_coord_distance: Vec<((&'a Coordinate, &'a Coordinate), f64)>,
-    junction_count: usize
+    sorted_coord_distance: Vec<((&'a Coordinate, &'a Coordinate), i64)>,
+    junction_count: usize,
 ) -> HashMap<&'a Coordinate, usize> {
     let mut circuits: HashMap<&'a Coordinate, usize> = HashMap::new();
 
@@ -62,13 +112,13 @@ fn create_circuits<'a>(
                         circuits.insert(coords.0, 0);
                         circuits.insert(coords.1, 0);
                     }
-                },
+                }
                 (Some(zero_index), None) => {
                     circuits.insert(coords.1, zero_index);
-                },
+                }
                 (None, Some(one_index)) => {
                     circuits.insert(coords.0, one_index);
-                },
+                }
                 (Some(zero_index), Some(one_index)) => {
                     for circuit_id in circuits.values_mut() {
                         if circuit_id == &zero_index {
@@ -83,12 +133,12 @@ fn create_circuits<'a>(
 
 fn coordinates_vec_to_sorted_distance_vec(
     coordinates: &Vec<Coordinate>,
-) -> Vec<((&Coordinate, &Coordinate), f64)> {
-    let mut distance_map: HashMap<(&Coordinate, &Coordinate), f64> = HashMap::new();
+) -> Vec<((&Coordinate, &Coordinate), i64)> {
+    let mut distance_map: HashMap<(&Coordinate, &Coordinate), i64> = HashMap::new();
     for i in 0..coordinates.len() {
         for coord in coordinates {
             if coord != &coordinates[i] {
-                let distance: f64 = coord.compute_distance(&coordinates[i]);
+                let distance: i64 = coord.compute_distance(&coordinates[i]);
 
                 if !distance_map.contains_key(&(&coordinates[i], coord))
                     && !distance_map.contains_key(&(coord, &coordinates[i]))
@@ -100,11 +150,11 @@ fn coordinates_vec_to_sorted_distance_vec(
     }
 
     // sorting iter by distance
-    let mut vec_map: Vec<((&Coordinate, &Coordinate), f64)> = distance_map
+    let mut vec_map: Vec<((&Coordinate, &Coordinate), i64)> = distance_map
         .iter()
         .map(|(&coords, &distance)| (coords, distance))
         .collect();
-    vec_map.sort_by(|a, b| a.1.total_cmp(&b.1));
+    vec_map.sort_by(|a, b| a.1.cmp(&b.1));
     vec_map
 }
 
@@ -116,17 +166,17 @@ fn map_input_to_coordinates(input: &str) -> Vec<Coordinate> {
             let x = parts
                 .next()
                 .unwrap()
-                .parse::<i32>()
+                .parse::<i64>()
                 .expect("Must be a number");
             let y = parts
                 .next()
                 .unwrap()
-                .parse::<i32>()
+                .parse::<i64>()
                 .expect("Must be a number");
             let z = parts
                 .next()
                 .unwrap()
-                .parse::<i32>()
+                .parse::<i64>()
                 .expect("Must be a number");
             Coordinate { x, y, z }
         })
@@ -162,5 +212,10 @@ mod tests {
     #[test]
     fn test_puzzle_one() {
         assert_eq!(40, puzzle_one(EXAMPLE_INPUT, 10));
+    }
+
+    #[test]
+    fn test_puzzle_two() {
+        assert_eq!(25272i32, puzzle_two(EXAMPLE_INPUT));
     }
 }
